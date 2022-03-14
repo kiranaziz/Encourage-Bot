70f691d95c19afcadbd8126b5662ecf0d7408eac
Kiran Aziz <86931096+kiranaziz@users.noreply.github.com>
2022-03-14 11:15:30 -0400

    Add files via upload

---
+++ Encouragment Discord Bot (VS Code Version)/main.py
@@ -0,0 +1,147 @@
+import discord
+import os 
+import requests #The requests module allows code to make an HTTP request
+                #to get data from the API, and the API returns JSON.
+import json     #The json module bc it will be easier to access the data from the API
+import random   #Random module used for random messages.
+import sqlite3
+from keep_alive import keep_alive
+
+#Creates an instance of a client
+client = discord.Client()
+
+conn = sqlite3.connect('encouragements.db')
+c = conn.cursor()
+# c.execute("""CREATE TABLE encouragements (
+#           encouragements text
+#           )""")
+
+# c.execute("INSERT INTO encouragements VALUES ('You are very cool!')")
+# conn.commit()
+
+# c.execute("INSERT INTO encouragements VALUES ('You are special!')")
+# conn.commit()
+
+c.execute("SELECT * FROM encouragements")
+
+print(c.fetchall())
+
+conn.commit()
+
+
+#List of words that relate to being sad. Anytime a user uses one of 
+#these words, the bot will return a response.
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+#A list of encouraging statements
+starter_encouragements = [
+  "Cheer up",
+  "Hang in there!",
+  "You are a great person!"
+]
+
+responding = True
+
+#Returns a random quote from the API
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random") #API url
+    json_data = json.loads(response.text)
+    #'q' - represents the quote
+    #'a' - represents the author of the quote
+    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+    return(quote)
+
+def update_encouragements(encouraging_message):
+  c.execute("SELECT * FROM encouragements WHERE encouragements=:message", {'message': encouraging_message})
+  result = c.fetchone()
+  conn.commit()
+  
+  if result is None:
+    c.execute("INSERT INTO encouragements VALUES (:message)", {'message': encouraging_message})
+    conn.commit()
+
+def delete_encouragement(encouraging_message):
+  c.execute("DELETE from encouragements WHERE encouragements=:message", {'message': encouraging_message})
+  conn.commit()
+
+#When the bot is on and ready to use, it will print a message
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+#on_message event triggers each time a message is received.
+@client.event
+async def on_message(message):
+    global responding
+    msg = message.content
+    
+    if message.author == client.user:
+        return
+    if msg.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+    if responding:
+      c.execute("SELECT * FROM encouragements")
+      result = c.fetchall()
+      conn.commit()
+      options = starter_encouragements
+      if result:
+        options = options + result
+      #Bot will go through every word in the list of sad words
+      #and see if any sad words are in the message
+      if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(options))
+
+    #User command to add new encouraging message to the messages database.
+    if msg.startswith("$new"):
+      #Since the command will be in the format "$new happy to see you"
+      #we want the message to only include the words after the command $new,
+      #so we split the message into an array of size 2. Then take the value,
+      #at index 1 and use that as the encouraging message.
+      encouraging_message = msg.split("$new ", 1)[1]
+      update_encouragements(encouraging_message)
+      await message.channel.send("New encouraging message added.") 
+
+    #User command to deleter an encouraging message 
+    #from the messages database using a number to indicate the index.
+    if msg.startswith("$del"):
+      c.execute("SELECT * FROM encouragements")
+      result = c.fetchall()
+      conn.commit()
+      encouragements = []
+      if result:
+        encouraging_message = msg.split("$del ", 1)[1]
+        delete_encouragement(encouraging_message)
+        c.execute("SELECT * FROM encouragements")
+        result2 = c.fetchall()
+        conn.commit()
+        encouragements = result2
+      await message.channel.send(encouragements)
+
+    if msg.startswith("$list"):
+      c.execute("SELECT * FROM encouragements")
+      result = c.fetchall()
+      conn.commit()
+      encouragements = []
+      if result:
+        encouragements = result
+      await message.channel.send(encouragements)
+    
+    #format of message = $responding false/true
+    if msg.startswith("$responding"):
+      value = msg.split("$responding ", 1)[1]
+      if value.lower() == "true":
+        responding = True
+        await message.channel.send("Responding is on.")
+      else:
+        responding = False
+        await message.channel.send("Responding is off.")
+        
+
+
+#Runs the bot.
+#keep_alive()
+client.run(os.environ['TOKEN'])
+
+conn.close()
